c8c768a900bd7206ae3967209757e064dd74276a
Luiz Pachioni <lopachioni@gmail.com>
2022-08-25 22:50:50 -0300

    criando array para guardar os resultadors, e criando função para exibir os resultados

--- Calculadora.js
+++ Calculadora.js
@@ -1,25 +1,44 @@
 var numA = 0
 var numB = 0
-var resultado = []
+var resultados = []
+var contador = 0
 var continuar = 1
 
 while (continuar == 1) {
     numA = parseInt(prompt("Insira um valor para A"))
     numB = parseInt(prompt("Insira um valor para B"))
 
-    var opcao = prompt("Qual operação deseja fazer? 1 - Somar / 2 - Subtrair / 3 - Multiplicar/ 4 - Dividir")
+    var opcao = prompt("Qual operação deseja fazer? 1 - Somar / 2 - Subtrair / 3 - Multiplicar / 4 - Dividir / 5 - Exibir Resultados")
 
     if (opcao == 1) {
-        console.log("O resultado da soma é: " + Somar(numA, numB))
+        resultados[contador] = Somar(numA, numB)
+        console.log("O resultado da soma é: " + resultados[contador])
+        contador++
     } else if (opcao == 2) {
-        console.log("O resultado da subtração é: " + Subtrair(numA, numB))
+        resultados[contador] = Subtrair(numA, numB)
+        console.log("O resultado da subtração é: " + resultados[contador])
+        contador++
     } else if (opcao == 3) {
-        console.log("O resultado da multiplicação é: " + Multiplicar(numA, numB))
+        resultados[contador] = Multiplicar(numA, numB)
+        console.log("O resultado da multiplicação é: " + resultados[contador])
+        contador++
     } else if (opcao == 4) {
-        console.log("O resultado da divisão é: " + Dividir(numA, numB))
+        resultados[contador] = Dividir(numA, numB)
+        console.log("O resultado da divisão é: " + resultados[contador])
+        contador++
+    } else if (opcao == 5) {
+        ExibirHistorico()
+    } else {
+        continuar = 2
     }
 
     continuar = prompt("Deseja fazer outra operação? 1 - Sim / 2 - Não")
+}
+
+function ExibirHistorico() {
+    for (var index = 0; index < contador; index++) {
+        console.log(resultados[index])
+    }
 }
 
 function Somar(numUm, numDois) {
