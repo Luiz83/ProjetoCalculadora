9e579ba0c00dbed2d6b623b4057c95ba8dd4c28d
Luiz Pachioni <lopachioni@gmail.com>
2022-08-25 22:19:42 -0300

    criando função de soma

--- Calculadora.js
+++ Calculadora.js
@@ -3,3 +3,9 @@ var numB = 0
 
 numA = parseInt(prompt("Insira um valor para A"))
 numB = parseInt(prompt("Insira um valor para B"))
+
+function Somar(numUm, numDois) {
+    return numUm + numDois
+}
+var resultado = Somar(numA, numB)
+console.log(resultado)
