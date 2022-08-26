93f8794083d0cd07ec874012b8cf5be80dff8cea
Luiz Pachioni <lopachioni@gmail.com>
2022-08-25 22:34:30 -0300

    criando menu de opções do usuário, e fazendo lógica para repetição

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,32 @@
 var numA = 0
 var numB = 0
 var resultado = []
+var continuar = 1
 
-numA = parseInt(prompt("Insira um valor para A"))
-numB = parseInt(prompt("Insira um valor para B"))
+while (continuar == 1) {
+    numA = parseInt(prompt("Insira um valor para A"))
+    numB = parseInt(prompt("Insira um valor para B"))
+
+    var opcao = prompt("Qual operação deseja fazer? 1 - Somar / 2 - Subtrair / 3 - Multiplicar/ 4 - Dividir")
+
+    if (opcao == 1) {
+        console.log("O resultado da soma é: " + Somar(numA, numB))
+    } else if (opcao == 2) {
+        console.log("O resultado da subtração é: " + Subtrair(numA, numB))
+    } else if (opcao == 3) {
+        console.log("O resultado da multiplicação é: " + Multiplicar(numA, numB))
+    } else if (opcao == 4) {
+        console.log("O resultado da divisão é: " + Dividir(numA, numB))
+    }
+
+    continuar = prompt("Deseja fazer outra operação? 1 - Sim / 2 - Não")
+}
 
 function Somar(numUm, numDois) {
     return numUm + numDois
 }
 
-function Susbtrair(numUm, numDois) {
+function Subtrair(numUm, numDois) {
     return numUm - numDois
 }
 
