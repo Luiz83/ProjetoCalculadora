932f072d2cf4cba34ffcbdd5a0845d45a92077d4
Luiz Pachioni <lopachioni@gmail.com>
2022-08-25 22:21:18 -0300

    criando função de subtrair

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,6 @@
 var numA = 0
 var numB = 0
+var resultado = []
 
 numA = parseInt(prompt("Insira um valor para A"))
 numB = parseInt(prompt("Insira um valor para B"))
@@ -7,5 +8,7 @@ numB = parseInt(prompt("Insira um valor para B"))
 function Somar(numUm, numDois) {
     return numUm + numDois
 }
-var resultado = Somar(numA, numB)
-console.log(resultado)
+
+function Susbtrair(numUm, numDois) {
+    return numUm - numDois
+}
